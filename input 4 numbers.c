d4f22b288126c4595d6ca4201b3bf75fd7e792e8
AdelleStuuu~ <adelledoesstuff@gmail.com>
2025-09-20 22:41:15 +0800

    formatting

--- input 4 numbers.c
+++ input 4 numbers.c
@@ -7,7 +7,9 @@ int main()
 
     scanf("%d%d%d%d", &horseshoe1, &horseshoe2, &horseshoe3, &horseshoe4);
 
-    UniqueHorseshoes = (horseshoe1 != horseshoe2 && horseshoe1 != horseshoe3 && horseshoe1 != horseshoe4) + (horseshoe2 != horseshoe3 && horseshoe2 != horseshoe4) + (horseshoe3 != horseshoe4);
+    UniqueHorseshoes = (horseshoe1 != horseshoe2 && horseshoe1 != horseshoe3 && horseshoe1 != horseshoe4) +
+                       (horseshoe2 != horseshoe3 && horseshoe2 != horseshoe4) +
+                       (horseshoe3 != horseshoe4);
 
     amountToBuy = 4 - (UniqueHorseshoes + 1);
 
