a242923df671242f195ded017315784dee6e5796
AdelleStuuu~ <adelledoesstuff@gmail.com>
2025-10-07 23:50:32 +0800

    checks whether a character that was inputted was either a consonant or a vowel

---
+++ PRACTICE SET 2/consonant or vowel.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+int main()
+{
+    char letter;
+    bool isVowel;
+    printf("Insert a Letter: ");
+    scanf("%c", &letter);
+
+    switch (letter)
+    {
+    case 'a':
+    case 'A':
+    case 'e':
+    case 'E':
+    case 'i':
+    case 'I':
+    case 'o':
+    case 'O':
+    case 'u':
+    case 'U':
+        isVowel = true;
+        break;
+    default:
+        isVowel = false;
+    }
+
+    if (isVowel)
+    {
+        printf("It is a vowel!");
+    }
+    else
+    {
+        printf("It is a consonant!");
+    }
+
+    printf("\n\n");
+}
