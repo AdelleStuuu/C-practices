99dc8e3ee4ee8a12e539b528704ef5281503a39f
AdelleStuuu~ <adelledoesstuff@gmail.com>
2025-10-08 01:00:37 +0800

    program that checks how much a group has to split their costs

---
+++ FROM AN AMIGO/cost splitting.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main()
+{
+    float numOfPeople, peopleBackingOut, costOfPurchase;
+
+    printf("Enter number of people in the group! ");
+    scanf("%f", &numOfPeople);
+    printf("Enter the cose of the present! ");
+    scanf("%f", &costOfPurchase);
+    printf("Enter the number of people backing out! ");
+    scanf("%f", &peopleBackingOut);
+
+    float initialContribution = costOfPurchase / numOfPeople;
+    float additionalCostOfPurchase = initialContribution * peopleBackingOut / (numOfPeople - peopleBackingOut);
+    float newContributionCosts = initialContribution + additionalCostOfPurchase;
+
+    printf("\nOriginal contribution per person: %.2f", initialContribution);
+    printf("\nAdditional contribution per person: %.2f", additionalCostOfPurchase);
+    printf("\nNew contribution cost per person: %.2f", newContributionCosts);
+
+    printf("\n\n");
+    return 0;
+}
